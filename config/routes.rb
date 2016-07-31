e15af01e01368283064ac23af4140449d310784c
Faye Cheng <fayehcheng@gmail.com>
2016-07-31 13:08:00 -0700

    Deleted index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   
-  root 'static_pages#index'
-
-  get '/index', to: 'static_pages#index'
+  root 'static_pages#about'
 
   get '/about', to: 'static_pages#about'
 
